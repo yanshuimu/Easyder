33876fa2f00a5a06e6909aa05c61ad3ec333ebc1
yanshuimu <593692553@qq.com>
2021-01-10 14:43:12 +0800

    调整

--- Easyder/Utils/EDKeyChainUtil.h
+++ Easyder/Utils/EDKeyChainUtil.h
@@ -11,27 +11,27 @@
 @interface KeyChainUtil : NSObject
 
 /**
- * setter
+ * 以key-value键值对储存至钥匙串
  */
 + (void)setString:(NSString*)string forKey:(NSString*)key;
 
 /**
- * setter（base64加密）
+ * 生成base64加密字符串value，再以key-value键值对储存至钥匙串
  */
 + (void)setStringWithBase64Encryption:(NSString*)string forKey:(NSString*)key;
 
 /**
- * getter
+ * 根据key从钥匙串中获取对应的value
  */
 + (NSString*)stringForKey:(NSString*)key;
 
 /**
- * getter（base解密）
+ * 根据key从钥匙串中获取对应的value，再经过base64解密
  */
 + (NSString*)stringForKeyWithBase64Decryption:(NSString*)key;
 
 /**
- * 移除
+ * 移除key-value键值对
  */
 + (void)removeItemForKey:(NSString*)key;
 
